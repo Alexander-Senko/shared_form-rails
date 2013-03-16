c0d41758eadb79c3ceb1837905f35bc4b5d049a3
Alexander Senko <Alexander.Senko@gmail.com>
2013-03-16 19:29:24 +0700

    Added basic support of decorators.

--- app/helpers/stub_helper.rb
+++ app/helpers/stub_helper.rb
@@ -3,17 +3,25 @@ require 'stub'
 module StubHelper
 	# TODO: remove the patching
 	def resource_stub
-		@resource_stub ||= Stub::Template::Prototype.new resource_class.new { |record|
-			class << record
-				def new_record?
-					false
-				end
+		@resource_stub ||= begin
+			stub = Stub::Template::Prototype.new resource_class.new { |record|
+				class << record
+					def new_record?
+						false
+					end
 
-				def id
-					'#{id}'
+					def id
+						'#{id}'
+					end
 				end
+			}
+
+			if stub.respond_to? :decorator_class then
+				stub.decorator_class.new stub
+			else
+				stub
 			end
-		}
+		end
 	end
 
 	# TODO: remove postprocessing
