f210c7009fff9b79023ce75edabe1cba93baea4b
Alexander Senko <Alexander.Senko@gmail.com>
2013-03-30 18:58:07 +0700

    API: added support of multiple objects to `shared_form_for`.

--- app/helpers/shared_form_helper.rb
+++ app/helpers/shared_form_helper.rb
@@ -3,9 +3,9 @@ module SharedFormHelper
 
 	delegate :locales, :multilingual?, :to => SharedForm
 
-	def shared_form_for record, options={}, &block
-		simple_form_for record, {
-			html: shared_form_html_options_for(record),
+	def shared_form_for resources, options = {}, &block
+		simple_form_for shared_form_object(resources), {
+			html: shared_form_html_options_for(resources),
 		}.deep_merge(options) do |form|
 			render layout: '/shared_form', object: form, &block
 		end
@@ -13,17 +13,25 @@ module SharedFormHelper
 
 	private
 
-	def shared_form_html_options_for resource
+	def shared_form_html_options_for resources
 		{
 			class: 'shared',
 			lang:  I18n.locale,
 			name:  resource_instance_name,
 			data: {
-				objects: [ resource.persisted? && resource ].compact.to_json,
+				objects: [ resources ].flatten.to_json,
 				:'thumbnail-template'    => render_stub(resource_stub).to_str,
 				:'resource-url-template' => polymorphic_url(resource_stub,  format: :json),
 				:'collection-url'        => polymorphic_url(resource_class, format: :json),
 			}
 		}
+	end
+
+	def shared_form_object resources
+		if resources.respond_to? :map then
+			resource_class.new
+		else
+			resources
+		end
 	end
 end
